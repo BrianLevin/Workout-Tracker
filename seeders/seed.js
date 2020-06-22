67f7f2759d7536ba6747f0a175c78d81d899236c
Brian Levin <bml201095@gmail.com>
2020-06-21 22:03:46 -0400

    changed my local host to the number and added the dataport number for mongo db to connect to the server

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,7 +1,7 @@
 let mongoose = require("mongoose");
 let db = require("../models");
 
-mongoose.connect("mongodb://localhost/workout", {
+mongoose.connect("mongodb://127.0.0.1:27017/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
