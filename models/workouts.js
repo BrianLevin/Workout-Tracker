ae03bcddc49b92577695e1a0ca8d616b90f95717
Brian Levin <bml201095@gmail.com>
2020-06-24 19:25:28 -0400

    made a server js file with a get workouts request

--- models/workouts.js
+++ models/workouts.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
-const exerciseSchema = new Schhema({
+const exerciseSchema = new Schema({
     type: {
         type: String,
         required: true
