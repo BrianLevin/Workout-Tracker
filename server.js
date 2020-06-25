115ad0653874aebe0cfd0999c400ffbd65337ee8
Brian Levin <bml201095@gmail.com>
2020-06-24 21:52:18 -0400

    added routes to the server js file

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/workout",
 
 app.get("/api/workouts", (req, res) => {
     db.Workout.find({})
-        .then(dbBook => {
+        .then(Workout => {
             res.json(Workout);
         })
         .catch(err => {
@@ -27,7 +27,45 @@ app.get("/api/workouts", (req, res) => {
         });
 });
 
+app.put('/api/workouts/:id', function (req, res) {
+    var id = req.params.id;
+    var exercise = req.body;
+    db.Workout.findByIdAndUpdate(id, { $push: { exercises: exercise } })
+    res.end("successfully updated workout");
+    // update the workout by its id (use Mongoose) and add the new exercise
+    // using the the push function (Mongoose)
+});
 
+app.post("/api/workouts/", ({ body }, res) => {
+    db.Workout.create(body)
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+
+});
+
+app.get("/api/workouts/range", (req, res) => {
+    db.Workout.find({})
+        .then(Workout => {
+            res.json(Workout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+app.get("/exercise", (req, res) => {
+    res.sendFile(__dirname + "/public/exercise.html")
+});
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html")
+});
+
+app.get("/stats", (req, res) => {
+    res.sendFile(__dirname + "/public/stats.html")
+});
 
 
 app.listen(PORT, () => {
