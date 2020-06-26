78fda45f240801667acdc073f5f212e99052ceab
Brian Levin <bml201095@gmail.com>
2020-06-26 19:24:56 -0400

    added changes to routes so it can work

--- server.js
+++ server.js
@@ -8,12 +8,14 @@ const db = require("./models");
 
 const app = express();
 
-app.use(logger("dev"));
+//app.use(logger("dev"));
 
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 app.use(express.static("public"));
+// workout , Workout, or workouts?
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/workout", { useNewUrlParser: true });
 
@@ -29,26 +31,37 @@ app.get("/api/workouts", (req, res) => {
 
 app.put('/api/workouts/:id', function (req, res) {
     var id = req.params.id;
+
     var exercise = req.body;
-    db.Workout.findByIdAndUpdate(id, { $push: { exercises: exercise } })
+
+
+    db.Workout.findByIdAndUpdate(id, { $push: { exercises: exercise } }, { new: true, runValidators: true })
+        .then(Workout => {
+            console.log(Workout);
+        })
+        .catch(err => {
+            console.log(err);
+        });
     res.end("successfully updated workout");
     // update the workout by its id (use Mongoose) and add the new exercise
     // using the the push function (Mongoose)
 });
 
 app.post("/api/workouts/", ({ body }, res) => {
-    db.Workout.create(body)
+
+    db.Workout.create({ day: new Date(), ...body })
         .then(dbWorkout => {
             res.json(dbWorkout);
         })
         .catch(err => {
+
             res.json(err);
         });
 
 });
 
 app.get("/api/workouts/range", (req, res) => {
-    db.Workout.find({})
+    db.Workout.find({}).limit(7)
         .then(Workout => {
             res.json(Workout);
         })
