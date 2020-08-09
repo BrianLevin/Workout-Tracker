14c757dea8150daa4aac8b7e72887886a6dbc643
Brian Levin <bml201095@gmail.com>
2020-08-09 13:02:04 -0400

    changed my server.js file to use the roues folder

--- server.js
+++ server.js
@@ -1,3 +1,34 @@
+
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://127.0.0.1:27017/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+// routes
+app.use(require("./routes/api.js"));
+app.use(require("./routes/view.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
+
+/*
+
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
@@ -84,3 +115,5 @@ app.get("/stats", (req, res) => {
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
 });
+
+*/
